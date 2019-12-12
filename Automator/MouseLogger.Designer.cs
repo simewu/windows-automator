aad1eaa1bb4807516efeaa54e3f9ff655cab005b
Simeon <simeonwuthier@gmail.com>
2019-12-11 23:07:16 -0700

    Commit committed

---
+++ Automator/MouseLogger.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Automator
+{
+    partial class MouseLogger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MouseLogger));
+            this.list = new System.Windows.Forms.ListBox();
+            this.toggle = new System.Windows.Forms.Button();
+            this.table = new System.Windows.Forms.TableLayoutPanel();
+            this.table.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // list
+            // 
+            this.list.BackColor = System.Drawing.Color.Teal;
+            this.list.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.list.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.list.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.list.ForeColor = System.Drawing.Color.White;
+            this.list.FormattingEnabled = true;
+            this.list.ItemHeight = 20;
+            this.list.Location = new System.Drawing.Point(0, 30);
+            this.list.Margin = new System.Windows.Forms.Padding(0);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(327, 332);
+            this.list.TabIndex = 0;
+            this.list.SelectedIndexChanged += new System.EventHandler(this.list_SelectedIndexChanged);
+            // 
+            // toggle
+            // 
+            this.toggle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toggle.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toggle.Location = new System.Drawing.Point(0, 0);
+            this.toggle.Margin = new System.Windows.Forms.Padding(0);
+            this.toggle.Name = "toggle";
+            this.toggle.Size = new System.Drawing.Size(327, 30);
+            this.toggle.TabIndex = 1;
+            this.toggle.Text = "Start Listener";
+            this.toggle.UseVisualStyleBackColor = true;
+            this.toggle.Click += new System.EventHandler(this.toggle_Click);
+            // 
+            // table
+            // 
+            this.table.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.table.ColumnCount = 1;
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.table.Controls.Add(this.list, 0, 1);
+            this.table.Controls.Add(this.toggle, 0, 0);
+            this.table.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.table.Location = new System.Drawing.Point(0, 0);
+            this.table.Name = "table";
+            this.table.RowCount = 2;
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.table.Size = new System.Drawing.Size(327, 362);
+            this.table.TabIndex = 2;
+            // 
+            // MouseLogger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(327, 362);
+            this.Controls.Add(this.table);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MouseLogger";
+            this.Text = "Mouse Logger";
+            this.Load += new System.EventHandler(this.MouseLogger_Load);
+            this.table.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Button toggle;
+        private System.Windows.Forms.TableLayoutPanel table;
+    }
+}
